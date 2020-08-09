8d9a7cf5aa4bc82e5ee2fbc38a96f919bface1fa
Spacetana <46649267+Spacetana@users.noreply.github.com>
2020-08-09 07:52:42 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-exports.TOKEN = "NzMwMTE4NTIxNjIyNjI2Mzk1.XwS13A.u2E9ohn6tfQTZn-tFesimwNCJwg";
-exports.PREFIX = "r!";
-exports.VERSION = "0.7.6"; 
-exports.WHITELIST = ["509115921156014081", "553543583710445568"];
